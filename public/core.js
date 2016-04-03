9896d30470a70cb95266a1b85e9afe2f31f33ba8
Yi-Wei Ang <yiweiang@gmail.com>
2016-04-03 17:37:57 +0800

    Update Angular

--- public/core.js
+++ public/core.js
@@ -1,5 +1,5 @@
 // public/core.js
-var scotchTodo = angular.module('financeApp', []);
+var scotchTodo = angular.module('financeApp', []).controller('mainController', mainController);
 
 function mainController($scope, $http) {
   $scope.formData = {};
